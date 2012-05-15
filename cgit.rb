8c0b9f13a44054cd0b5cc9bc49071a3b114cfde1
shaohua yang <s.h.yang1984@gmail.com>
2012-05-15 12:41:26 +0800

    v0.9

--- cgit.rb
+++ cgit.rb
@@ -1,9 +1,15 @@
 require 'formula'
 
+class Git < Formula
+  url 'http://kernel.org/pub/software/scm/git/git-1.7.4.4.tar.bz2'
+  homepage 'http://git-scm.com'
+  md5 '1313f71d62fa100b32fa313769a85f2a'
+end
+
 class Cgit <Formula
-  url 'http://hjemli.net/git/cgit/snapshot/cgit-0.8.3.4.tar.bz2'
+  url 'http://hjemli.net/git/cgit/snapshot/cgit-0.9.tar.bz2'
   homepage 'http://hjemli.net/git/cgit/'
-  md5 '712e4d3013d754aa5752e0101188cf32'
+  md5 'b55f953c5c8a418bca72b3de3c297270'
 
   def install
     # cgit need the source code of git in its `git/' subfolder
@@ -11,7 +17,6 @@ class Cgit <Formula
     Git.new.brew { system 'cp -r * ' + cgit_staging_dir + '/git/' }
 
     # build and install
-    system "make DESTDIR=#{prefix} NEEDS_LIBICONV=1 install cgit"
-    bin.install 'cgit'
-  end 
+    system "make DESTDIR=#{prefix} NEEDS_LIBICONV=1 install"
+  end
 end
