007c3848e22cf31bcf5dd9dcec83dd184c69b7ec
shaohua yang <shaohua0110@yahoo.com>
2013-12-01 14:42:10 +0800

    url rewriting could break when python upgrades, use http redirect

--- server.py
+++ server.py
@@ -6,14 +6,8 @@ from http.server import HTTPServer, CGIHTTPRequestHandler
 
 os.environ['CGIT_CONFIG'] = 'cgitrc' # in current working directory
 
-class RequestHandler(CGIHTTPRequestHandler):
-    def is_cgi(self):
-        cgi = '/cgit.cgi'
-        self.cgi_info = cgi, self.path[len(cgi):] # setting the PATH_INFO correctly eliminates the need of URL rewriting
-        return self.path == '/' or self.path.startswith(cgi)
-
 port = 8000
-httpd = HTTPServer(('127.0.0.1', port), RequestHandler)
+httpd = HTTPServer(('127.0.0.1', port), CGIHTTPRequestHandler)
 
 print('start serving on port %d...' % port)
 
