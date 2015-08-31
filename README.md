5a72106ec0b3c4a42ad85508b62dfc5361bd86a6
syang <syang@local>
2015-08-31 12:20:55 +0800

    update upstream

--- README.md
+++ README.md
@@ -5,7 +5,7 @@ This is a python cgi server (server.py) just for serving cgit.
 
 cgit is a web frontend for git repositories (http://hjemli.net/git/cgit/)
 
-cgit.cgi is a precompiled mac 64-bit binary. (make get-git all NO_GETTEXT=1)
+cgit.cgi is a precompiled mac 64-bit binary. (make get-git all NO_GETTEXT=1 NO_LUA=1)
 
 Ignore lighttpd.conf, Makefile and cgit.rb (for Homebrew https://github.com/mxcl/homebrew/)
 
--- cgit.rb
+++ cgit.rb
@@ -1,22 +1,16 @@
 require 'formula'
 
-class Git < Formula
-  url 'https://github.com/git/git/tarball/v1.7.4.5'
-  homepage 'http://git-scm.com'
-  md5 '9b5aa25a5758a616f2c2b17630448b33'
-end
-
 class Cgit <Formula
-  url 'http://hjemli.net/git/cgit/snapshot/cgit-0.9.0.3.tar.bz2'
-  homepage 'http://hjemli.net/git/cgit/'
-  md5 '3d5481bf8a507694108f0996b8bde235'
+  url 'http://git.zx2c4.com/cgit/snapshot/cgit-0.11.2.tar.xz'
+  homepage 'http://git.zx2c4.com/cgit/'
+  sha256 '2e126e770693d7296c7eb5eb83b809410aef29870bfe8f54da072a3f4d813e3b'
 
   def install
     # cgit need the source code of git in its `git/' subfolder
     cgit_staging_dir = Dir.pwd
-    Git.new.brew { system 'cp -r * ' + cgit_staging_dir + '/git/' }
 
     # build and install
-    system "make DESTDIR=#{prefix} NEEDS_LIBICONV=1 install"
+    system 'make get-git'
+    system "make DESTDIR=#{prefix} NO_GETTEXT=1 NO_LUA=1 install"
   end
 end
